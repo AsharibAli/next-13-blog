8c15b07df718f2528b97e60af8e3474e866fa1d7
Asharib <asharibarain4@gmail.com>
2023-02-12 13:42:09 +0500

    Testing Blog App

---
+++ app/page.tsx
@@ -0,0 +1,36 @@
+import Image from "next/image";
+import { Inter } from "@next/font/google";
+import styles from "./page.module.css";
+import Link from "next/link";
+
+import { posts } from "../data/posts";
+
+const inter = Inter({ subsets: ["latin"] });
+
+type Post = {
+  id: string;
+  title: string;
+  date: string;
+};
+
+export default function Home() {
+  return (
+    <main className={styles.main}>
+      <div className={styles.center}>
+        <h1>Home</h1>
+      </div>
+      <div>
+        {posts.map(({ id, title, date }: Post) => (
+          <>
+            <Link href={`/posts/${id}`}>{title}</Link>
+            <br />
+            <p>{date}</p>
+            <br />
+          </>
+        ))}
+      </div>
+      <Link href="/posts">Go to Posts</Link>
+      <Link href="/account">Accounts</Link>
+    </main>
+  );
+}
